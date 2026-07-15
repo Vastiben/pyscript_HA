946f7c22550d81fc8146bd5d8c3d2cbeee573147
Vastiben <98217116+Vastiben@users.noreply.github.com>
2026-07-15 22:03:14 +0200

    Update fusionsolar_pyscript.py

--- fusionsolar/fusionsolar_pyscript.py
+++ fusionsolar/fusionsolar_pyscript.py
@@ -25,7 +25,7 @@ CONFIG = {
     "referer": "https://uni004eu5.fusionsolar.huawei.com/uniportal/pvmswebsite/assets/build/cloud.html?app-id=smartpvms",
     # Paste the complete Cookie request header value here, for example:
     # "cookie": "SESSION=...; other_cookie=...",
-    "cookie": "PASTE_EDGE_NETWORK_COOKIE_HERE",
+    "cookie": "JSESSIONID=520FBE4CD997FE338902E66458AA5663",
     "update_every": "period(now, 2min)",
 }
 
