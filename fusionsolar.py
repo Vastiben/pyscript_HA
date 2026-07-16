b4d5586293db3f27f1e8ea19884dfbe1613ef30d
Vastiben <98217116+Vastiben@users.noreply.github.com>
2026-07-16 06:29:10 +0200

    cleanup: delete obsolete fusionsolar.py (migrated to apps/fusionsolar/)

--- fusionsolar.py
+++
@@ -1,3 +0,0 @@
-# This file is obsolete — FusionSolar has been migrated to apps/fusionsolar/
-# See /config/pyscript/apps/fusionsolar/__init__.py
-# This file can be safely deleted from /config/pyscript/
