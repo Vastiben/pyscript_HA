430b80a02ce5151026c3a39ed643966ac3506bfd
Vastiben <98217116+Vastiben@users.noreply.github.com>
2026-07-26 17:25:23 +0200

    Create bootstrap.py

---
+++ bootstrap.py
@@ -0,0 +1,15 @@
+"""
+Point d'entrée unique Pyscript.
+Importe tous les modules contenant des triggers.
+"""
+
+from health.webhook import *
+from health.sleep_score import *
+
+from infrastructure.watchdog import *
+from infrastructure.wireguard import *
+
+from solar.fusionsolar import *
+from solar.health import *
+
+from github_sync.pull import *
