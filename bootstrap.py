3e8b45eeb78cc3c7a2341fd7f05b8f38dae93696
Vastiben <98217116+Vastiben@users.noreply.github.com>
2026-07-26 17:44:22 +0200

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -3,13 +3,13 @@ Point d'entrée unique Pyscript.
 Importe tous les modules contenant des triggers.
 """
 
-from health.webhook import *
-from health.sleep_score import *
+#from health.webhook import *
+#from health.sleep_score import *
 
-from infrastructure.watchdog import *
-from infrastructure.wireguard import *
+#from infrastructure.watchdog import *
+#from infrastructure.wireguard import *
 
-from solar.fusionsolar import *
-from solar.health import *
+#from solar.fusionsolar import *
+#from solar.health import *
 
-from github_sync.pull import *
+from github_sync import *
