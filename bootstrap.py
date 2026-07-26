fa4ca3a8af44458f79c2a680aca2845a062ca4dd
Vastiben <98217116+Vastiben@users.noreply.github.com>
2026-07-26 18:36:52 +0200

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -4,5 +4,11 @@ Importe tous les modules contenant des triggers.
 """
 
 
-from github_sync import *
-from infrastructure import *
+from github_sync.pull import *
+from github_sync.github_logs import *
+
+from infrastructure.system_commands import *
+from infrastructure.telegram_commands import *
+from infrastructure.watchdogs import *
+from infrastructure.wireguard_watchdog import *
+
