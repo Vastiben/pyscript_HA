fc0649a7809a5c5b874ee42da384a9ebf1cec36b
Vastiben <98217116+Vastiben@users.noreply.github.com>
2026-07-27 18:30:40 +0200

    Update pull.py

--- modules/github_sync/pull.py
+++ modules/github_sync/pull.py
@@ -10,7 +10,7 @@ def _notify(msg, chat_id=None):
     """
     data = {"message": str(msg)}
     if chat_id:
-        data["target"] = [chat_id]
+        data["chat_id"] = [chat_id]
     service.call("telegram_bot", "send_message", **data)
 
 
